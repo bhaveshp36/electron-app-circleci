38acd7f3c281ea2c822a61e92f6222ff4f6f57ea
bhavesh <bhaveshpurohit994@gmail.com>
2022-11-30 13:59:25 +0530

    update

---
+++ dependcies.sh
@@ -0,0 +1,8 @@
+$ sudo apt-get install build-essential clang libdbus-1-dev libgtk-3-dev 
+                       libnotify-dev libasound2-dev libcap-dev 
+                       libcups2-dev libxtst-dev 
+                       libxss1 libnss3-dev gcc-multilib g++-multilib curl 
+                       gperf bison python3-dbusmock openjdk-8-jre
+
+sudo apt-get install dpkg fakeroot yarn rpm-build
+
